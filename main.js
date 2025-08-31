06fcd196623fcab9562d258f76bec5148a762b38
DavidDoyle20 <davestar80@gmail.com>
2025-08-31 08:48:57 -0500

    added error response

--- main.js
+++ main.js
@@ -1,43 +1,57 @@
 const https = require('https');
 
 exports.handler = async (event) => {
-    const intent = event.request.intent;
-    const text = intent.slots.text.value;
+    try {
+        const intent = event.request.intent;
+        const text = intent.slots.text.value;
 
-    const data = JSON.stringify({
-        type: "todo",
-        text: text
-    });
-
-    const options = {
-        hostname: "yourserver.com",
-        port: 443,
-        path: "/api/print",
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Content-Length": Buffer.byteLength(data)
-        }
-    };
-
-    await new Promise((resolve, reject) => {
-        const req = https.request(options, (res) => {
-            res.on('data', () => { });
-            res.on('end', resolve);
+        const data = JSON.stringify({
+            type: "todo",
+            text: text
         });
-        req.on('error', reject);
-        req.write(data);
-        req.end();
-    });
 
-    return {
-        version: "1.0",
-        response: {
-            outputSpeech: {
-                type: "PlainText",
-                text: `Printing your todo: ${text}`
-            },
-            shouldEndSession: true
-        }
-    };
+        const options = {
+            hostname: "test.com",
+            port: 443,
+            path: "/api/print",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": Buffer.byteLength(data)
+            }
+        };
+
+        await new Promise((resolve, reject) => {
+            const req = https.request(options, (res) => {
+                res.on('data', () => { });
+                res.on('end', resolve);
+            });
+            req.on('error', reject);
+            req.write(data);
+            req.end();
+        });
+
+        return {
+            version: "1.0",
+            response: {
+                outputSpeech: {
+                    type: "PlainText",
+                    text: `Printing your todo: ${text}`
+                },
+                shouldEndSession: true
+            }
+        };
+    } catch (err) {
+        console.error("Error:", err);
+        return {
+            version: "1.0",
+            response: {
+                outputSpeech: {
+                    type: "PlainText",
+                    text: "Sorry, I couldn’t print that."
+                },
+                shouldEndSession: true
+            }
+        };
+    }
 };
